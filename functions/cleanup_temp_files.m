20b25384d64ab9966c28ae54a207fdd9ea7542da
Brandon Snyder <its.brandon96@yahoo.com>
2025-11-12 17:52:54 -0500

    Added SPDX-License-Identifier to all files

--- functions/cleanup_temp_files.m
+++ functions/cleanup_temp_files.m
@@ -1,3 +1,23 @@
+% SPDX-License-Identifier: GPL-3.0-or-later
+% EyeSort - Region-aware eye-tracking event labeling for EEGLAB
+% Copyright (C) 2025 Eye Movements & Cognition Lab (USF)
+% Copyright (C) 2025 Brandon Snyder, Sara Milligan, Elizabeth Schotter
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+% Author: Brandon Snyder
+
 function cleanup_temp_files(batchFilePaths)
 % CLEANUP_TEMP_FILES - Clean up temporary files created during processing
 %
